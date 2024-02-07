d9eed0432759e3aed0721e9435098882190ac6e1
jaxe93 <jaxe93work@gmail.com>
2024-02-06 19:18:42 -0500

    added src files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-// import config from "./components/Config";
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
 
-// function App() 
+import ActionProvider from "./components/ActionProvider";
+import MessageParser from "./components/MessageParser";
+import config from "./components/Config";
 
-// export default App;
+function App() {
+  return (
+    <div className="App flex justify-center items-center h-screen bg-slate-900">
+      <header className="App-header w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
+        <Chatbot
+          config={config}
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
